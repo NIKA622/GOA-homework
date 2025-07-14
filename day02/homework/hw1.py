3cd80103f3ac11f9457b129a125b4950590bb6cd
NIKA622 <nikatalaxadze09@gmail.com>
2025-07-14 20:44:43 +0400

    1

---
+++ day02/homework/hw1.py
@@ -0,0 +1,41 @@
+#1
+x=2043
+y=300
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+#2
+#string
+name1="mayvala"
+name2="parna"
+country="japan"
+judoka="ono shohei"
+
+#int
+age=16
+month=12
+budget=2000
+year=2025
+
+#float
+height=1.80
+distance=20.50
+weight=45.543
+percentage=99.99
+
+#3 სტრინგებიერტმანეტს დაემატა ანუ მოხდა კონკატინაცია
+firstname="Gia"
+lastname="jonjoli"
+print(firstname + lastname)
+
+#4
+word1="become."
+word2="better."
+word3="don't."
+word4="be."
+word5="weak."
+print(word1 + word2 + word3 + word4 + word5)
+
+#5
