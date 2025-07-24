bdfd348762a823bc1fc420ee39bedf70ee7f66d9
NIKA622 <nikatalaxadze09@gmail.com>
2025-07-24 15:37:00 +0400

    1

---
+++ day04/homework/hw1.py
@@ -0,0 +1,64 @@
+#2)"input"--> არის ინპორმაცია რპმელიც შედის კომპიუტერში---"output" არის ინპორმაცია რომელიც გამოდისკომპიუტერიდან
+
+
+name=input()
+print(type (name))
+print(name)
+
+drink="tea"
+number=20
+height=4.5
+print(type(drink))
+print(type(number))
+print(type(height))
+
+
+
+
+#4) თიოთეული მონაცემთა ტიპისთვის (str,int,float), შექმენით 5 ცვლადი და დაუწერეთ კომენტარი თუ რომელ მონაცემთა ტიპს ინახავს ცვლადი.
+best_judoka="ono shohei" #str
+car_model="BMW"  #str
+food="pizza"   #str
+item="phone"  #str
+actor="jake gyllenhaal"  #str
+
+
+x=10   #int
+y=30   #int
+age=34  #int
+people=2000   #int
+day=10   #INT
+
+
+height=1.90   #float
+distance=20.4   #float
+widht=3.2   #float
+meeter=6.4   #float
+lenght=3.7   #float
+
+
+
+
+#5) აიღეთ 3 ცვლადი, შეინახეთ განსხავებული მონაცემთა ტიპები (str,int,float), შემდეგ type ინსტრუქციის გამოყენებით შეამოწმეთ, თუ რომელ მონაცემთა ტიპს ინახავს ცვლადი.
+name="nika"
+num=12
+meeter=1.2
+print(type(num))
+print(type(meeter))
+print(type(name))
+
+#6) მომხმარებელს შემოატანინეთ ორი სიტყვა, შეინახეთ ისინი ცვლადებში, მოახდინეთ მათი კონკატინაცია და დაბეჭდეთ.
+word1=input("enter your name")
+word2=input("enter lastname")
+print(word1 + word2)
+
+
+
+
+#7) მომხმარებელს შემოატანინეთ 5 რიცხვი სხვადასხვა დამოუკიდებელი input-ების სახით, თქვენი დავალებაა დაბეჭდოთ მათი საშუალო არითმეტიკული.
+num1=int(input())
+num2=int(input())
+num3=int(input())
+num4=int(input())
+num5=int(input())
+print(num1+num2+num3+num4+num5)
