96917bb64256f7016f55c385234aacb7131050d5
NIKA622 <nikatalaxadze09@gmail.com>
2025-10-07 00:53:38 +0400

    1

--- day24/homework/hw.py
+++ day24/homework/hw.py
@@ -36,3 +36,56 @@ for i in range(6):
         print("შენ აირჩიე კატა" if animals[i]=="cat" else "შენ აირჩიე თხა" if animals[i]=="goat" else "სხვა ცხოველი აირჩიე")
         break
 else: print("არასწორი ინდექსი შეიყვანე")
+#6
+cities = ["Tbilisi", "Batumi", "Kutaisi", "Rustavi", "Telavi", "Zugdidi"]
+
+a = int(input("შეიყვანე პირველი ინდექსი: "))
+b = int(input("შეიყვანე მეორე ინდექსი: "))
+
+if a < b:
+    print("ამ ინდექსებზე არის:", cities[a], "და", cities[b])
+elif b < a:
+    print("შეცვალე ინდექსები ადგილებით")
+    print("ამ შემთხვევაში ესენი იქნებიან:", cities[b], "და", cities[a])
+else:
+    print("ორივე ინდექსი ერთნაირია")
+    print("ეს არის:", cities[a])
+#7
+word = input("შეიყვანე სიტყვა: ")
+
+if word[0].lower() == "a":
+    print("სიტყვა იწყება a-თი")
+elif word[-1].lower() == "z":
+    print("სიტყვა მთავრდება z-ით")
+else:
+    print("სიტყვა არც a-თი იწყება და არც z-ით მთავრდება")
+#8
+word = input("შეიყვანე სიტყვა: ")
+
+if word[0] == word[-1]:
+    print("პირველი და ბოლო ასო ერთნაირია")
+else:
+    print("პირველი და ბოლო ასო განსხვავებულია")
+#9
+letters = "agivorsbgitr"
+
+goga = letters[2] + letters[0] + letters[2] + letters[0]
+print("პირველი სიტყვა:", goga)
+
+saba = letters[7] + letters[0] + letters[1] + letters[0]
+print("მეორე სიტყვა:", saba)
+
+bativar = letters[1] + letters[0] + letters[3] + letters[1] + letters[5] + letters[0] + letters[6]
+print("მესამე სიტყვა:", bativar)
+#10
+text = "giorgi"
+
+print("for ციკლით:")
+for letter in text:
+    print(letter)
+
+print("\nwhile ციკლით:")
+i = 0
+while i < len(text):
+    print(text[i])
+    i += 1
---
+++ day28/classwork/cw.py
@@ -0,0 +1,14 @@
+
+lst1 = [1,2,3,4,5,6,7,8,9,10]
+print(lst1[5:])   
+
+
+lst2 = list(range(1, 21))
+print(lst2[:-9])
+
+
+
+
+lst3 = list(range(1, 13))
+num = int(input("შეიყვანე რიცხვი: "))
+print(lst3[2:num])  
