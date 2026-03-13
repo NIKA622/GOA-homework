a0eb66fdf298ba2b6437858bdf093e40e9eed551
NIKA622 <nikatalaxadze09@gmail.com>
2026-03-13 15:34:56 +0400

    1

---
+++ day73/homework/hw.py
@@ -0,0 +1,37 @@
+
+#3
+# def quote(fighter):
+#     fighter = fighter.lower()
+    
+#     if fighter == "george saint pierre":
+#         return "I am not impressed by your performance."
+#     else:
+#         return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+
+
+
+#4
+
+# def replace_exclamation(s):
+#     vowels = "aeiouAEIOU"   
+#     result = ""             
+
+#     for char in s:          
+#         if char in vowels:  
+#             result += "!"   
+#         else:
+#             result += char  
+
+#     return result           
+
+#5
+# def stringy(size):
+#     result = "" 
+
+#     for i in range(size):  
+#         if i % 2 == 0:    
+#             result += "1"  
+#         else:              
+#             result += "0"  
+
+#     return result          
